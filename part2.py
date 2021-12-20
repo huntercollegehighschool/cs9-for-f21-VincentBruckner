d022877c41b9f89cbaa212e5e64b91785d901d44
Vincent Bruckner <94186747+VincentBruckner@users.noreply.github.com>
2021-12-20 23:44:08 +0000

    For this one, I made a for loop with i in the range of 1, num + 1 and then I cubed i using the ** command. I also had another variable, sum1, which equaled to sum1 + cube for each team the loop ran. In the end, I printed sum1

--- part2.py
+++ part2.py
@@ -8,3 +8,15 @@ For example, if the user types in 4, the program should print 100 (since 1^3 + 2
 """
 
 #write your code here
+num = int(input("What cube are you looking for? "))
+
+cube = 1
+sum1 = 0
+
+for i in range(1, num + 1):
+  cube = i ** 3
+  sum1 = sum1 + cube
+  
+cube2 = sum1
+
+print(cube2)
